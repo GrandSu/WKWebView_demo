31e237ffc19f35918c17d4b6d42930a34246a9dd
Li Yong <btt365com@gmail.com>
2018-08-24 16:24:03 +0800

    Initial Commit

---
+++ WKWebView_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebView_demo
+//
+//  Created by bet001 on 2018/8/24.
+//  Copyright © 2018年 GrandSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebView_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebView_demo
+//
+//  Created by bet001 on 2018/8/24.
+//  Copyright © 2018年 GrandSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
